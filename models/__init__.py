3ed0324950ea4d51b72f582d863d10e68e1d8df5
MYAT THU <myat90thu@gmail.com>
2026-02-04 23:15:20 +0630

    CHange odoo store structure for dir

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import stock_transport_request
-from . import stock_transport_request_line
-from . import stock_transport_approval_rule_line
-from . import stock_picking_inherit
-from . import stock_warehouse_inherit
--- models/stock_warehouse_inherit.py
+++
@@ -1,4 +0,0 @@
-from odoo import fields, models
-
-class StockWarehouse(models.Model):
-    _inherit = 'stock.warehouse'
